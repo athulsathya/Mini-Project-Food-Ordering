559fa06bf82ed8ffc3d286276e0d43786b5b7806
Athul Sathya <sathyaathul92@gmail.com>
2026-05-18 00:17:16 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,14 @@ import {
 import Layout from "./components/Layout";
 import GuestRoute from "./components/GuestRoute";
 import ProtectedRoute from "./components/ProtectedRoute";
-
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/ContactUs";
 import CustomerOrders from "./pages/CustomerOrders";
-import Privacy from "./pages/Privacy";
-import Error from "./pages/Error";
-
 import Cart from "./components/Cart";
 import FoodList from "./components/Foodlist";
-
 import AdminLayout from "./components/AdminLayout";
 import Add from "./pages/Add";
 import List from "./pages/List";
@@ -72,7 +67,6 @@ function AppRoutes() {
           path: "customer-orders",
           element: <CustomerOrders />,
         },
-        { path: "privacy", element: <Privacy /> },
       ],
     },
 
@@ -121,11 +115,7 @@ function AppRoutes() {
       ],
     },
 
-    /* Error Route */
-    {
-      path: "*",
-      element: <Error />,
-    },
+    
   ]);
 
   return <RouterProvider router={router} />;
